b2b823183197fd0cf7d075eb3a4ae25ffce54e98
Bernhard Berchtold <berchtold@mathematik.ch>
2020-01-06 15:51:35 +0100

    Trennung von Binomialverteilung, falls nötig

--- zufallstuff.js
+++ zufallstuff.js
@@ -50,5 +50,5 @@ txt[1]="Platon Körper";
 var Wahlbi=new Array();var txtbi=new Array();
 Wahlbi[0]='<a href="/anwendungenmath/wkeit/binomialvert.html"><img src="/img/binomialverteilung.png" class="a"></a>';
 Wahlbi[1]='<a href="/anwendungenmath/wkeit/approx_bin_norm.html"><img src="/img/approx.jpg" class="a"></a>';
-txtbi[0]="Binomialverteilung";
+txtbi[0]="Binomial&shy;verteilung";
 txtbi[1]="Approximation<br>";
