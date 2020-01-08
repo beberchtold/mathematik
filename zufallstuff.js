24332ea2f4fd9d42bf57a997b7ef0211660fd732
Bernhard Berchtold <berchtold@mathematik.ch>
2020-01-08 11:14:05 +0100

    3 weitere Mathematiker aufgenommen, zufallstuff optimiert

--- zufallstuff.js
+++ zufallstuff.js
@@ -1,73 +1,77 @@
 // Zufallsspiel
-var Spiel=new Array();var Link=new Array();
-Spiel[0]='<a href="anwendungenmath/blackjack/"><img src="/img/blackjack.jpg" class="a"></a>';
-Spiel[1]='<a href="/anwendungenmath/solitaire/"><img src="/img/solitaire.png" class="a"></a>';
-Spiel[2]='<a href="/spiele/Sokoban/"><img src="/img/sokoban.png" class="a"></a>';
-Spiel[3]='<a href="/spiele/hanoi_mit_grafik/"><img src="/img/hanoi.png" class="a"></a>';
-Spiel[4]='<a href="/puzzle/SixteenPuzzle/"><img src="/img/sixteenpuzzle.png" class="a"></a>';
-Spiel[5]='<a href="/puzzle/puzzle23/"><img src="/img/puzzle23.png" class="a"></a>';
-Spiel[6]='<a href="/anwendungenmath/pento/"><img src="/img/pentominos.png" class="a"></a>';
-Spiel[7]='<a href="/puzzle/puzzle14/"><img src="/img/puzzle14.png" class="a"></a>';
-Spiel[8]='<a href="/puzzle/puzzle12/"><img src="/img/theseus_klein.png" class="a"></a>';
-Spiel[9]='<a href="/puzzle/puzzle20/"><img src="/img/puzzle20.png" class="a"></a>';
-Spiel[10]='<a href="/puzzle/puzzle26/"><img src="/puzzle/puzzle26/solut26.jpg" class="a"></a>';
-Spiel[11]='<a href="/spiele/euromillions/"><img src="/img/euromillions.png" class="a"></a>';
-Spiel[12]='<a href="/spiele/swisslotto/"><img src="/img/swisslotto.png" class="a"></a>';
-Spiel[13]='<a href="/spiele/numbers/"><img src="/img/numbers.png" class="a"></a>';
-Spiel[14]='<a href="/spiele/sudoku/"><img src="/img/sudoku.png" class="a"></a>';
-Spiel[15]='<a href="/spiele/sudoku_x/"><img src="/img/sudokux.png" class="a"></a>';
+var Spiel=[
+'<a href="anwendungenmath/blackjack/"><img src="/img/blackjack.jpg" class="a"></a>',
+'<a href="/anwendungenmath/solitaire/"><img src="/img/solitaire.png" class="a"></a>',
+'<a href="/spiele/Sokoban/"><img src="/img/sokoban.png" class="a"></a>',
+'<a href="/spiele/hanoi_mit_grafik/"><img src="/img/hanoi.png" class="a"></a>',
+'<a href="/puzzle/SixteenPuzzle/"><img src="/img/sixteenpuzzle.png" class="a"></a>',
+'<a href="/puzzle/puzzle23/"><img src="/img/puzzle23.png" class="a"></a>',
+'<a href="/anwendungenmath/pento/"><img src="/img/pentominos.png" class="a"></a>',
+'<a href="/puzzle/puzzle14/"><img src="/img/puzzle14.png" class="a"></a>',
+'<a href="/puzzle/puzzle12/"><img src="/img/theseus_klein.png" class="a"></a>',
+'<a href="/puzzle/puzzle20/"><img src="/img/puzzle20.png" class="a"></a>',
+'<a href="/puzzle/puzzle26/"><img src="/puzzle/puzzle26/solut26.jpg" class="a"></a>',
+'<a href="/spiele/euromillions/"><img src="/img/euromillions.png" class="a"></a>',
+'<a href="/spiele/swisslotto/"><img src="/img/swisslotto.png" class="a"></a>',
+'<a href="/spiele/numbers/"><img src="/img/numbers.png" class="a"></a>',
+'<a href="/spiele/sudoku/"><img src="/img/sudoku.png" class="a"></a>',
+'<a href="/spiele/sudoku_x/"><img src="/img/sudokux.png" class="a"></a>'
+];
 
 // Zufallsknacknüsse
-Link[0]='<a href="/puzzle/puzzle1.html"><img src="/img/puzzle1.png" class="a"></a>';
-Link[1]='<a href="/puzzle/puzzle2.html"><img src="/img/puzzle2.png" class="a"></a>';
-Link[2]='<a href="/puzzle/puzzle3.html"><img src="/img/puzzle3.png" class="a"></a>';
-Link[3]='<a href="/puzzle/puzzle4.html"><img src="/img/puzzle4.png" class="a"></a>';
-Link[4]='<a href="/puzzle/puzzle5.html"><img src="/puzzle/img/puzzle5.png" class="a"></a>';
-Link[5]='<a href="/puzzle/puzzle6.html"><img src="/img/puzzle6.png" class="a"></a>';
-Link[6]='<a href="/puzzle/puzzle7/"><img src="/img/door.png" class="a"></a>';
-Link[7]='<a href="/puzzle/puzzle9.html"><img src="/img/puzzle9.png" class="a"></a>';
-Link[8]='<a href="/puzzle/puzzle10.html"><img src="/img/puzzle10.jpg" class="a"></a>';
-Link[9]='<a href="/puzzle/puzzle11.html"><img src="/img/puzzle11.jpg" class="a"></a>';
-Link[10]='<a href="/puzzle/puzzle15.html"><img src="/img/puzzle15.png" class="a"></a>';
-Link[11]='<a href="/puzzle/puzzle17.html"><img src="/puzzle/img/puzzle17.png" class="a"></a>';
-Link[12]='<a href="/puzzle/puzzle18.html"><img src="/img/puzzle18.jpg" class="a"></a>';
-Link[13]='<a href="/puzzle/puzzle19.html"><img src="/img/puzzle19.jpg" class="a"></a>';
-Link[14]='<a href="/puzzle/puzzle21.html"><img src="/img/puzzle21.png" class="a"></a>';
-Link[15]='<a href="/puzzle/puzzle22.html"><img src="/img/puzzle22.jpg" class="a"></a>';
-Link[16]='<a href="/puzzle/puzzle24.html"><img src="/img/puzzle24.jpg" class="a"></a>';
-Link[17]='<a href="/puzzle/puzzle31.html"><img src="/img/puzzle31.png" class="a"></a>';
-Link[18]='<a href="/puzzle/puzzle33.html"><img src="/puzzle/img/puzzle33_hund.gif" class="a"></a>';
-Link[19]='<a href="/puzzle/puzzle35.html"><img src="/img/puzzle35.jpg" class="a"></a>';
+var Link=[
+'<a href="/puzzle/puzzle1.html"><img src="/img/puzzle1.png" class="a"></a>',
+'<a href="/puzzle/puzzle2.html"><img src="/img/puzzle2.png" class="a"></a>',
+'<a href="/puzzle/puzzle3.html"><img src="/img/puzzle3.png" class="a"></a>',
+'<a href="/puzzle/puzzle4.html"><img src="/img/puzzle4.png" class="a"></a>',
+'<a href="/puzzle/puzzle5.html"><img src="/puzzle/img/puzzle5.png" class="a"></a>',
+'<a href="/puzzle/puzzle6.html"><img src="/img/puzzle6.png" class="a"></a>',
+'<a href="/puzzle/puzzle7/"><img src="/img/door.png" class="a"></a>',
+'<a href="/puzzle/puzzle9.html"><img src="/img/puzzle9.png" class="a"></a>',
+'<a href="/puzzle/puzzle10.html"><img src="/img/puzzle10.jpg" class="a"></a>',
+'<a href="/puzzle/puzzle11.html"><img src="/img/puzzle11.jpg" class="a"></a>',
+'<a href="/puzzle/puzzle15.html"><img src="/img/puzzle15.png" class="a"></a>',
+'<a href="/puzzle/puzzle17.html"><img src="/puzzle/img/puzzle17.png" class="a"></a>',
+'<a href="/puzzle/puzzle18.html"><img src="/img/puzzle18.jpg" class="a"></a>',
+'<a href="/puzzle/puzzle19.html"><img src="/img/puzzle19.jpg" class="a"></a>',
+'<a href="/puzzle/puzzle21.html"><img src="/img/puzzle21.png" class="a"></a>',
+'<a href="/puzzle/puzzle22.html"><img src="/img/puzzle22.jpg" class="a"></a>',
+'<a href="/puzzle/puzzle24.html"><img src="/img/puzzle24.jpg" class="a"></a>',
+'<a href="/puzzle/puzzle31.html"><img src="/img/puzzle31.png" class="a"></a>',
+'<a href="/puzzle/puzzle33.html"><img src="/puzzle/img/puzzle33_hund.gif" class="a"></a>',
+'<a href="/puzzle/puzzle35.html"><img src="/img/puzzle35.jpg" class="a"></a>'
+];
 
 // Lissajou oder PlatonKörper
-var Wahl=new Array();var txt=new Array();
-Wahl[0]='<a href="/anwendungenmath/lissajou/"><img src="/img/lissajou.jpg" class="a"></a>';
-Wahl[1]='<a href="/anwendungenmath/PlatonKoerper/"><img src="/img/ikosaeder.png" class="a"></a>';
-txt[0]="Lissajou Figur";
-txt[1]="Platon Körper";
+var Wahl=[
+'<a href="/anwendungenmath/lissajou/"><img src="/img/lissajou.jpg" class="a"></a>',
+'<a href="/anwendungenmath/PlatonKoerper/"><img src="/img/ikosaeder.png" class="a"></a>'
+];
+var txt=["Lissajou Figur","Platon Körper"];
 
 // Binomialverteilung oder Approximation
-var Wahlbi=new Array();var txtbi=new Array();
-Wahlbi[0]='<a href="/anwendungenmath/wkeit/binomialvert.html"><img src="/img/binomialverteilung.png" class="a"></a>';
-Wahlbi[1]='<a href="/anwendungenmath/wkeit/approx_bin_norm.html"><img src="/img/approx.jpg" class="a"></a>';
-txtbi[0]="Binomial&shy;verteilung";
-txtbi[1]="Approximation<br>Binomial&shy;verteilung";
+var Wahlbi=[
+'<a href="/anwendungenmath/wkeit/binomialvert.html"><img src="/img/binomialverteilung.png" class="a"></a>',
+'<a href="/anwendungenmath/wkeit/approx_bin_norm.html"><img src="/img/approx.jpg" class="a"></a>'
+];
+var txtbi=["Binomial&shy;verteilung","Approximation<br>Binomial&shy;verteilung"];
 
 // fractal oder Mandelbrot
-var fractal=new Array();var txtfrac=new Array();
-fractal[0]='<a href="/anwendungenmath/fractal"><img src="/img/farn.png" class="a"></a>';
-fractal[1]='<a href="/anwendungenmath/fractal/julia/julia.html"><img src="/img/mandelbrot.png" class="a"></a>';
-txtfrac[0]="Fraktale";
-txtfrac[1]="Mandelbrot";
+var fractal=[
+'<a href="/anwendungenmath/fractal"><img src="/img/farn.png" class="a"></a>',
+'<a href="/anwendungenmath/fractal/julia/julia.html"><img src="/img/mandelbrot.png" class="a"></a>'
+];
+var txtfrac=["Fraktale","Mandelbrot"];
 
 //Zufallsmathematiker
-var math=new Array();var txtmath=new Array();
-math[0]='<a href="mathematiker/euler.html"><img src="/img/euler.jpg" class="b"></a>';
-math[1]='<a href="/mathematiker/newton.html"><img src="/img/newton.jpg" class="b"></a>';
-math[2]='<a href="/mathematiker/fermat.html"><img src="/img/fermat.jpg" class="b"></a>';
-math[3]='<a href="/mathematiker/gauss.html"><img src="/img/gauss.jpg" class="b"></a>';
-txtmath[0]="Euler";
-txtmath[1]="Newton";
-txtmath[2]="Fermat";
-txtmath[3]="Gauss";
+var math=[
+'<a href="mathematiker/euler.html"><img src="/img/euler.jpg" class="b"></a>',
+'<a href="/mathematiker/newton.html"><img src="/img/newton.jpg" class="b"></a>',
+'<a href="/mathematiker/fermat.html"><img src="/img/fermat.jpg" class="b"></a>',
+'<a href="/mathematiker/gauss.html"><img src="/img/gauss.jpg" class="b"></a>',
+'<a href="mathematiker/galois.html"><img src="/img/galois.jpg" class="b"></a>',
+'<a href="/mathematiker/goedel.html"><img src="/img/goedel.jpg" class="b"></a>',
+'<a href="/mathematiker/hilbert.html"><img src="/img/hilbert.jpg" class="b"></a>'
+];
+var txtmath=["Leonhard Euler","Isaac Newton","Pierre de Fermat","C. F. Gauss","Evariste Galois","Kurt Goedel","David Hilbert"];
 
