e735c8dfc694cc5fc4b44e9faa123ea22f2f0936
beberchtold <48598057+beberchtold@users.noreply.github.com>
2020-05-25 09:46:59 +0200

    ergänzt mit function infos(..,..)

--- script/menuopen.js
+++ script/menuopen.js
@@ -1,1 +1,2 @@
 function menu() {var neuFenster = window.open("/menu_js.html", "Menu","width=300,height=520,left=100,top=100");neuFenster.focus();}
+function infos(seite,name) {var neuFenster = window.open(seite,name,"width=350,height=600,left=100,top=100");neuFenster.focus();}
