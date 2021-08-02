89335f818c0c77dec096b00c20c80edf041abcce
beberchtold <48598057+beberchtold@users.noreply.github.com>
2021-08-02 17:20:31 +0200

    id closeicon

--- script/show-hide.js
+++ script/show-hide.js
@@ -1,1 +1,12 @@
-function ReverseDisplay(){"none"==document.getElementById("menu").style.display?(document.getElementById("menu").style.display="block",document.getElementById("content").style.display="none",window.document.images[0].src="/img/menu_closeicon.gif"):(document.getElementById("menu").style.display="none",document.getElementById("content").style.display="block",window.document.images[0].src="/img/menuicon.png")}
+function ReverseDisplay() {
+if(document.getElementById('menu').style.display == "none")
+  { document.getElementById('menu').style.display = "block";
+    document.getElementById('content').style.display = "none";
+    document.getElementById('menuicon').style.display = "none";
+	document.getElementById('closeicon').style.display = "inline-block"; }
+else
+  { document.getElementById('menu').style.display = "none";
+    document.getElementById('content').style.display = "block";
+    document.getElementById('closeicon').style.display = "none";
+    document.getElementById('menuicon').style.display = "inline-block";	}
+}
