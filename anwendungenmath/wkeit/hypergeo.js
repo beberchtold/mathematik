1b3addbd79d09d04a252b45591b487ed783fa800
beberchtold <48598057+beberchtold@users.noreply.github.com>
2023-11-19 15:05:17 +0100

    spiel eurodreams aufgenommen und dazugehörige Links Ergänzung im Resultat hypergeo Verteilung

--- anwendungenmath/wkeit/hypergeo.js
+++ anwendungenmath/wkeit/hypergeo.js
@@ -1,1 +1,128 @@
-var W,H;var m,r,n,a,b,mue,sigma;var wkeit=new Array();var summe;var canvas1;var initmade=false;window.onload=resizeCanvas;function resizeCanvas(){var c=document.getElementById("containercanvas");c.width=c.offsetWidth;W=c.width;H=W/2;if(initmade){resize1();if(n<=30){zeichne()}}else{init()}}function Bikoeff(c,f){var e;if(f>c){e=0}else{e=1}if(f>c-f){f=c-f}for(var d=1;d<=f;d++){e=e*(c-d+1)/d}return e}function calcwk(c,e,f){var d=Bikoeff(c,f);for(x=0;x<=f;x++){wkeit[x]=Bikoeff(e,x)*Bikoeff(c-e,f-x)/d}}function init(){initmade=true;window.addEventListener("resize",function(c){resizeCanvas()});canvas1=document.getElementById("myCanvas");resize1();ctx=canvas1.getContext("2d");rechne()}function resize1(){canvas1.width=W;canvas1.height=H}function rechne(){if(document.getElementById("m").value==""){alert("Wert für n eingeben!");return}m=parseInt(document.getElementById("m").value);if((m<=1)||(isNaN(m))){alert("falscher Wert für m");return}if(document.getElementById("r").value==""){alert("Wert für r eingeben!");return}r=parseInt(document.getElementById("r").value);if((r<=0)||(r>m)||(isNaN(r))){alert("falscher Wert für r");return}if(document.getElementById("n").value==""){alert("Wert für n eingeben!");return}n=parseInt(document.getElementById("n").value);if((n<=0)||(n>m)||(isNaN(n))){alert("falscher Wert für n");return}if(document.getElementById("a").value==""){alert("Wert für a eingeben!");return}a=parseInt(document.getElementById("a").value);if((a<0)||isNaN(a)||(a>n)||(a>r)){alert("falscher Wert für a");return}if(document.getElementById("b").value==""){alert("Wert für b eingeben!");return}b=parseInt(document.getElementById("b").value);if((isNaN(b))||(b>n)||(b<a)||(b>r)){alert("falscher Wert für b");return}summe=0;calcwk(m,r,n);for(var c=a;c<=b;c++){summe+=wkeit[c]}mue=n*r/m;sigma=Math.sqrt(n*r/m*(1-r/m)*(m-n)/(m-1));schreibeWerte();if(n<=30){zeichne()}else{ctx.clearRect(0,0,W,H)}}function zeichne(){ctx.clearRect(0,0,W,H);ctx.font="11px Arial";var d=W/(n+1);for(var c=0;c<=n;c++){if((c<a)||(c>b)){ctx.fillStyle="rgb(10,36,106)"}else{ctx.fillStyle="green"}ctx.fillRect(c*d,(H-20)*(1-2.5*wkeit[c]),d,(H-20)*2.5*wkeit[c]);ctx.fillText(c,c*d+d/2,H-5)}}function schreibeWerte(){var c=sigma*sigma;document.getElementById("M").innerHTML=m;document.getElementById("N").innerHTML=n;document.getElementById("R").innerHTML=r;document.getElementById("A").innerHTML=a;document.getElementById("B").innerHTML=b;document.getElementById("MU").innerHTML=runde(mue);document.getElementById("VAR").innerHTML=runde(c);document.getElementById("SIGMA").innerHTML=runde(sigma);document.getElementById("RESHY").innerHTML=Math.round(100000000*summe)/100000000}function runde(c){return Math.round(100000*c)/100000};
+  // Progamm zur hypergeometrischen Verteilung auf www.mathematik.ch
+  // November 2015: php-Programm vom Januar 2002 umgeschrieben auf html5 und Javascript
+  // November 2023: Ergänzungen bei Resultatangabe Wkeit (var summerez)
+  // copyright Bernhard Berchtold
+
+    var W,H;
+	var m,r,n,a,b,mue,sigma;
+	var wkeit=new Array();
+	var summe;
+	var summerez;
+	var canvas1;
+	var initmade=false;
+	
+window.onload=resizeCanvas;	
+    
+  function resizeCanvas() {
+    var canvs = document.getElementById("containercanvas");
+    canvs.width = canvs.offsetWidth;
+	W=canvs.width;
+	H=W/2;
+	if (initmade) {
+	  resize1();
+      if (n<=30) zeichne();
+	} else init();	
+  }
+  
+  function Bikoeff(A,B) {
+    var F;
+    if (B>A) F = 0;
+     else F = 1;
+    if (B > A - B) B = A - B;
+    for (var i=1;i<=B;i++)
+      F = F * (A - i + 1) / i ;
+    return F;
+  }
+
+  function calcwk(m,r,n) {
+    var Nenner=Bikoeff(m,n);
+    for (x=0;x<=n;x++)
+     wkeit[x] = Bikoeff(r,x) * Bikoeff(m-r,n-x)/Nenner;
+  }
+
+  function init() {
+    initmade=true;
+	window.addEventListener('resize', function(event){
+      resizeCanvas()
+    });
+    canvas1 = document.getElementById('myCanvas');
+	resize1();
+    ctx = canvas1.getContext('2d');
+    rechne();
+  }
+
+  function resize1() {
+	 canvas1.width=W; canvas1.height=H; 
+  }
+
+  function rechne() {    // und zeichne()
+    if (document.getElementById("m").value == '') 
+     {alert ("Wert für n eingeben!"); return;}
+    m=parseInt(document.getElementById("m").value);
+	if  ((m<=1) || (isNaN(m)) )
+     {alert ("falscher Wert für m"); return;}
+    if (document.getElementById("r").value == '') 
+     {alert ("Wert für r eingeben!"); return;}
+    r=parseInt(document.getElementById("r").value);
+	if ((r<=0) || (r>m) || (isNaN(r)) )
+     {alert ("falscher Wert für r"); return;}
+    if (document.getElementById("n").value == '') 
+     {alert ("Wert für n eingeben!"); return;}
+    n=parseInt(document.getElementById("n").value);
+	if ((n<=0) || (n>m) || (isNaN(n)) )
+     {alert ("falscher Wert für n"); return;}	
+    if (document.getElementById("a").value == '') 
+     {alert ("Wert für a eingeben!"); return;}
+    a=parseInt(document.getElementById("a").value);
+	if ((a<0) || isNaN(a) || (a>n) || (a>r))
+	  {alert ("falscher Wert für a"); return;}	
+    if (document.getElementById("b").value == '') 
+     {alert ("Wert für b eingeben!"); return;}
+	b=parseInt(document.getElementById("b").value);
+	if ( (isNaN(b)) || (b>n) || (b<a) || (b>r) )
+     {alert ("falscher Wert für b"); return;}
+	// calc Hypergeovert
+	summe = 0;
+    calcwk(m,r,n);
+	for (var x=a;x<=b;x++) summe += wkeit[x];
+	summerez=runde(1/summe);
+    mue=n*r/m;
+    sigma=Math.sqrt(n*r/m*(1-r/m)*(m-n)/(m-1))
+	schreibeWerte();
+	if (n<=30) zeichne();
+	  else ctx.clearRect(0,0,W,H);
+  }
+
+  function zeichne() {  // wird durch Drücken auf button ausgelöst
+    ctx.clearRect(0,0,W,H);
+    ctx.font="11px Arial";
+ 	var breite = W/(n+1);   	   
+    for (var x=0;x<=n;x++) {
+	  if ((x<a) || (x>b)) ctx.fillStyle = "rgb(10,36,106)";
+        else ctx.fillStyle = "green";
+      ctx.fillRect(x*breite,(H-20)*(1-2.5*wkeit[x]),breite,(H-20)*2.5*wkeit[x]);
+      ctx.fillText(x,x*breite+breite/2,H-5);
+	}
+  }
+  
+  function schreibeWerte() {
+	var varianz=sigma*sigma;
+    document.getElementById("M").innerHTML=m;	
+	document.getElementById("N").innerHTML=n;
+	document.getElementById("R").innerHTML=r;
+	document.getElementById("A").innerHTML=a;
+	document.getElementById("B").innerHTML=b;
+	document.getElementById("MU").innerHTML=runde(mue);
+	document.getElementById("VAR").innerHTML=runde(varianz);
+	document.getElementById("SIGMA").innerHTML=runde(sigma);
+	document.getElementById("RES").innerHTML=runde(1e6*summe)/1e6;
+	document.getElementById("RESHY").innerHTML=summerez;
+  }
+
+  function runde(x) {
+	return Math.round(100000*x)/100000;  
+  }
+
+
+
+ 
---
+++ spiele/eurodreams/eurodreams.js
@@ -0,0 +1,208 @@
+// Programm zur Simulation Eurodreams auf www.mathematik.ch
+// AUTHOR and Copyright: Bernhard Berchtold
+// November 2023
+
+   var n=1;
+   var z1,z2,z3,z4,z5,z6;
+   var s1;
+   var zahl=new Array(6);
+   const Kosten_pro_Tipp=3.0;
+
+  function ziehung()
+    {  var R=new Array(6); 	
+    	R[0]= Math.floor(40*Math.random())+1;
+        var anzahl=1;
+        while (anzahl<6) {
+           var gleich=false;
+           R[anzahl]= Math.floor(40*Math.random())+1;
+           for (var i=0; i<anzahl; i++)
+             if (R[anzahl]==R[i]) {gleich=true; break;}
+           if (!gleich) anzahl++;
+        }
+        // Ziehung einer Zahl (aus den Zahlen 1 bis 5)
+        R[6]=Math.floor(5*Math.random())+1;      
+        return R;
+    }
+
+ function vergleichzahlen(zahl,z1,z2,z3,z4,z5,z6)
+ { var a=0;
+   for (var i = 0;  i < 6;  i++)
+      if (zahl[i]==z1) a++;
+   for (i = 0;  i < 6;  i++)
+      if (zahl[i]==z2) a++;
+   for (i = 0;  i < 6;  i++)
+      if (zahl[i]==z3) a++;
+   for (i = 0;  i < 6;  i++)
+      if (zahl[i]==z4) a++;
+   for (i = 0;  i < 6;  i++)
+      if (zahl[i]==z5) a++;
+   for (i = 0;  i < 6;  i++)
+      if (zahl[i]==z6) a++;  
+  return a;	
+ }
+
+ function vergleichsterne(zahl,s1)
+ { var b=0;
+   if (zahl[6]==s1) b++;     
+   return b;	
+ }
+ 
+ function gewinn1(a,b) {
+   var gewinn_betrag=0;
+   if ((a==6) && (b==1)) gewinn_betrag=10666560;    // 22222 pro Monat während 30 Jahren  
+   if ((a==6) && (b==0)) gewinn_betrag=133320;    // 2222 pro Monat während 5 Jahren
+   if (a==5) gewinn_betrag=160;
+   if (a==4) gewinn_betrag=60;
+   if (a==3) gewinn_betrag=8;
+   if (a==2) gewinn_betrag=3;          
+   return gewinn_betrag;	
+ }
+
+ function ziehe() {
+   document.getElementById("txt").innerHTML="<h2>Resultate der Simulation Eurodreams</h2>";
+   n=parseInt(document.getElementById("n").value);
+   if (isNaN(n) || n<1) n=1; if (n>1000) n=1000;
+   var ok=chkFormular();
+   if (!ok) return;   
+   document.getElementById("n").value=n;
+   document.getElementById("z1").value=z1;
+   document.getElementById("z2").value=z2;
+   document.getElementById("z3").value=z3;
+   document.getElementById("z4").value=z4;
+   document.getElementById("z5").value=z5;
+   document.getElementById("z6").value=z6;
+   document.getElementById("s1").value=s1;
+   var einbezahlte_Summe=runde(n*Kosten_pro_Tipp);
+   var Gewinnsumme=0;
+   var gewinn=false;
+   for (var i=1; i<n+1; i++) {  // Ziehung
+    zahl = ziehung();
+    // Vergleich
+    var a = vergleichzahlen(zahl,z1,z2,z3,z4,z5,z6);
+    var b = vergleichsterne(zahl,s1);
+    if (a==1) var z=" Zahl richtig, "; else var z=" Zahlen richtig, ";
+    if (b==1) var s=" Glückszahl richtig"; else var s=" Glückszahl falsch"; 
+    if ((n>1) &&  (a>=3))
+        { gewinn=true;
+          document.getElementById("txt").innerHTML+="Spiel "+i+": "+a+z+s+"<br>";
+		  Gewinnsumme+=gewinn1(a,b);
+        }
+    if (n==1) {
+        document.getElementById("txt").innerHTML+="Die Zufallszahlen des Computers heissen:<br>";
+        document.getElementById("txt").innerHTML+=zahl[0]+"&nbsp;&nbsp;&nbsp;&nbsp;";
+        document.getElementById("txt").innerHTML+=zahl[1]+"&nbsp;&nbsp;&nbsp;&nbsp;";  
+        document.getElementById("txt").innerHTML+=zahl[2]+"&nbsp;&nbsp;&nbsp;&nbsp;";
+        document.getElementById("txt").innerHTML+=zahl[3]+"&nbsp;&nbsp;&nbsp;&nbsp;";
+		document.getElementById("txt").innerHTML+=zahl[4]+"&nbsp;&nbsp;&nbsp;&nbsp;";
+        document.getElementById("txt").innerHTML+=zahl[5]+"<br>";
+        document.getElementById("txt").innerHTML+="Die gezogene Glückszahl ist ";
+        document.getElementById("txt").innerHTML+=zahl[6]+"<br><br>";   
+        document.getElementById("txt").innerHTML+="<h3>"+a+z+s+"</h3>";
+        Gewinnsumme+=gewinn1(a,b); 		
+    }
+  }
+  Gewinnsumme=runde(Gewinnsumme);
+  if ((n>1) && (!gewinn)) document.getElementById("txt").innerHTML+="Sie haben leider in keinem der "+n+" Spiele gewonnen.<br><br>";  
+  var bilanz=runde(Gewinnsumme-einbezahlte_Summe);
+  document.getElementById("txt").innerHTML+="<br>Sie haben <b>"+einbezahlte_Summe+"</b> Franken eingesetzt<br>";
+  document.getElementById("txt").innerHTML+="Sie haben <b>"+Gewinnsumme+"</b> Franken gewonnen<br>"; 
+  if (bilanz>0)
+    document.getElementById("txt").innerHTML+="Sie haben einen Gewinn von <b>"+bilanz+"</b> Franken erzielt!<br>";
+  if (bilanz==0)
+    document.getElementById("txt").innerHTML+="Sie haben weder gewonnen noch verloren!<br>";
+  if (bilanz<0)
+    { bilanz=bilanz*(-1);
+      document.getElementById("txt").innerHTML+="Sie haben einen Verlust von <b>"+bilanz+"</b> Franken erlitten!<br>";   	
+    }
+
+ }
+ 
+ function generatenumbers()
+ {
+  var R =new Array(5);
+  R[0]= Math.floor(40*Math.random())+1;
+  anzahl=1;
+  while (anzahl<6) {
+     gleich=false;
+     R[anzahl]= Math.floor(40*Math.random())+1;
+     for (i=0; i<anzahl; i++)
+        if (R[anzahl]==R[i]) {gleich=true; break;}
+     if (!gleich) anzahl++;
+   }
+ document.getElementById("z1").value=R[0];
+ document.getElementById("z2").value=R[1];
+ document.getElementById("z3").value=R[2];
+ document.getElementById("z4").value=R[3];
+ document.getElementById("z5").value=R[4];
+ document.getElementById("z6").value=R[5];
+ }
+
+ function generatestar()
+ { s1=Math.floor(5*Math.random())+1;
+ document.getElementById("s1").value=s1;
+ }
+
+ function runde(x) {
+	return Math.round(10*x)/10; 
+ }
+ 
+ function chkFormular()
+ {
+  if (document.getElementById("z1").value == '') 
+     {alert ("Wert für z1 eingeben!");
+      return false};
+  z1 = parseInt(document.getElementById("z1").value); 
+  if  ((z1<1) || (z1>40))
+     {alert ("falscher Wert für z1");
+      document.getElementById("z1").value = ''; 
+      return false};    
+   if (document.getElementById("z2").value == '') 
+     {alert ("Wert für z2 eingeben!");
+      return false};
+  z2 = parseInt(document.getElementById("z2").value); 
+  if  ((z2<1) || (z2>40) || (z2==z1))
+     {alert ("falscher Wert für z2");
+      document.getElementById("z2").value = ''; 
+      return false}; 
+  if (document.getElementById("z3").value == '') 
+     {alert ("Wert für z3 eingeben!");
+      return false};
+  z3 = parseInt(document.getElementById("z3").value); 
+  if  ((z3<1) || (z3>40) || (z3==z1) || (z3==z2))
+     {alert ("falscher Wert für z3");
+      document.getElementById("z3").value = ''; 
+      return false};    
+  if (document.getElementById("z4").value == '') 
+     {alert ("Wert für z4 eingeben!");
+      return false};
+  z4 = parseInt(document.getElementById("z4").value); 
+  if  ((z4<1) || (z4>40) || (z4==z1) || (z4==z2) || (z4==z3))
+     {alert ("falscher Wert für z4");
+      document.getElementById("z4").value = ''; 
+      return false};    
+  if (document.getElementById("z5").value == '') 
+     {alert ("Wert für z5 eingeben!");
+      return false};
+  z5 = parseInt(document.getElementById("z5").value); 
+  if  ((z5<1) || (z5>40) || (z5==z1) || (z5==z2) || (z5==z3) || (z5==z4))
+     {alert ("falscher Wert für z5");
+      document.getElementById("z5").value = ''; 
+      return false};
+  if (document.getElementById("z6").value == '') 
+     {alert ("Wert für z6 eingeben!");
+      return false};
+  z6 = parseInt(document.getElementById("z6").value); 
+  if  ((z6<1) || (z6>40) || (z6==z1) || (z6==z2) || (z6==z3) || (z6==z4) || (z6==z5))
+     {alert ("falscher Wert für z6");
+      document.getElementById("z6").value = ''; 
+      return false};	  
+  if (document.getElementById("s1").value == '') 
+     {alert ("Wert für s1 eingeben!");
+      return false};
+  s1 = parseInt(document.getElementById("s1").value); 
+  if  ((s1<1) || (s1>5) )
+     {alert ("falscher Wert für s1");
+      document.getElementById("s1").value = ''; 
+      return false};
+  return true;	  
+ }
